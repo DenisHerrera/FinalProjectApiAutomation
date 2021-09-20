6d2dd3a66e4d5ca96d4d40ac19cc575fb2976c00
DenisHerrera <denis.d.herrera.r@gmail.com>
2021-09-20 07:52:25 -0400

    fixing dependencies

--- src/test/java/DenisTest.java
+++ src/test/java/DenisTest.java
@@ -1,42 +1,64 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.json.simple.JSONObject;
-import org.testng.annotations.Test;
+import org.junit.Before;
+import static org.hamcrest.MatcherAssert.*;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
 
 public class DenisTest {
-    @Test
-    public void test2(){
-        RestAssured.baseURI = "https://reqres.in/api";
 
-        RestAssured.
-                given().
-                get("/users?page=2").
-                then().
-                statusCode(200).
-                body("data[4].first_name",equalTo("George"));
-        //body("data.first_name", hasItems("George","Raquel"));
+    @Before
+    public void setup(){
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+        RestAssured.requestSpecification=new RequestSpecBuilder().setContentType(ContentType.JSON).build();
     }
 
     @Test
-    public void testPost(){
+    public void deleteUserTest() {
+        given().log().all()
+                .delete("/users/2")
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
+    }
+
+    @Test
+    public void patchUser(){
         JSONObject request =new  JSONObject();
 
         request.put("name","Denis");
         request.put("job","Quality Assurance");
 
-        RestAssured.baseURI = "https://reqres.in/api";
+        String nameUpdated= given()
+                .body(request.toJSONString())
+                .patch("users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .jsonPath()
+                .getString("job");
 
-        RestAssured.
-                given().
-                header("Content-Type","Application/json").
-                contentType(ContentType.JSON).
-                accept(ContentType.JSON).
-                body(request.toJSONString()).
-                when().
-                post("/users").
-                then().
-                statusCode(201);
+        assertThat(nameUpdated,equalTo("Quality Assurance"));
     }
 }
